ec40fe38d18b12c16715197ee013dd2097728482
MejdoubDev <simajdoubs@gmail.com>
2023-06-06 15:57:30 +0100

    args

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,7 +2,8 @@
 from sys import argv
 if __name__ == '__main__':
     num_arg = len(argv) - 1
-    print(f"{num_arg} {'argument' if num_arg == 1
-          else 'arguments'}{'.' if num_arg == 0 else ':'}")
+    arg_word = 'argument' if num_arg == 1 else 'arguments'
+    punctuation = '.' if num_arg == 0 else ':'
+    print(f"{num_arg} {arg_word}{punctuation}")
     for i in range(1, num_arg + 1):
         print(f"{i}{':'} {argv[i]}")
