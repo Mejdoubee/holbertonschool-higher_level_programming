3a0e670c5e0fda94bd97f3f2c54465f3d9917949
MejdoubDev <simajdoubs@gmail.com>
2023-06-08 13:07:49 +0100

    lists of lists

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for raw in matrix:
+        if len(raw) == 0:
+            print("")
         for i in range(len(raw)):
             if i == len(raw) - 1:
                 print("{:d}".format(raw[i]))
