207541ff0ca3541cb2f9b8e8238c74162a238ae5
MejdoubDev <simajdoubs@gmail.com>
2023-06-10 11:56:34 +0100

    simple squared

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-	return [[i**2 for i in raw] for raw in matrix]
+    return [[i**2 for i in raw] for raw in matrix]
+
+
+'''
+    new_list = []
+    for raw in matrix:
+        new = []
+        for i in range(len(raw)):
+            new.append(raw[i] ** 2)
+        new_list.append(new)
+    return new_list
+'''
