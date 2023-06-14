bdc547272850dfb69b7f7fcdc6815216c87e7a83
Mejdoubee <6235@holbertonstudents.com>
2023-06-14 15:01:19 +0100

    First class

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python3
+"""
+Author: [Ismail Mejdoub]
+Date: [06/14/2023]
+Description: This module defines the Square class.
+"""
+
+
 class Square:
+    """
+    A class that represents a square.
+
+    """
     pass
