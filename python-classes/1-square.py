fae12fe286be15acb366719b5a720220ce96f4b2
Mejdoubee <6235@holbertonstudents.com>
2023-06-14 20:17:09 +0100

    Sqaure with size

---
+++ python-classes/1-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Author: [Ismail Mejdoub]
+Date: [06/14/2023]
+Description: This module defines the Square class.
+"""
+
+
+class Square:
+    """
+    A class that represents a square.
+
+    """
+
+    def __init__(self, size):
+        """
+        Initialize a new Square:
+        Args:
+            size (int): The size of the new Square.
+        """
+        self.__size = size
