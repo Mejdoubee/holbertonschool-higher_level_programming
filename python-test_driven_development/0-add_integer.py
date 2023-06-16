4d128bf333c054d2eadefcf25c5e045456cf4212
Mejdoubee <6235@holbertonstudents.com>
2023-06-16 16:56:25 +0100

    Integers addition

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -14,8 +14,4 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     if type(b) not in (int, float):
         raise TypeError("b must be an integer")
-    if type(a) == float:
-        a = int(a)
-    if type(b) == float:
-        b = int(b)
-    return a + b
+    return int(a) + (b)
