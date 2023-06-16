9f1ea50e41a67346843312f862dc15b0a5e7a7a1
Mejdoubee <6235@holbertonstudents.com>
2023-06-16 12:42:48 +0100

    Integers addition

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -8,7 +8,7 @@ which adds two numbers together.
 
 def add_integer(a, b=98):
     """
-    Adds two numbers (a, b) together. Both must be int or float. Returns int sum.
+    Adds 2 numbers (a, b) together. Both must be int or float,Returns int sum.
     """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
