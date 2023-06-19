92b0d84543476540c7a5561438a829d322e87592
Mejdoubee <6235@holbertonstudents.com>
2023-06-19 01:47:19 +0100

    Eval is magic

---
+++ python-more_classes/4-rectangle.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+'''
+This module defines a rectangle
+'''
+
+
+class Rectangle:
+    '''
+    Class that defines a rectangle with width, height, area and perimeter
+    '''
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.height * self.width
+
+    def perimeter(self):
+        if self.width == 0 or self.height == 0:
+            return 0
+        else:
+            return (self.width + self.height) * 2
+
+    def __str__(self):
+        rectang = ""
+        if self.__width != 0 or self.__width != 0:
+            for i in range(self.__height):
+                rectang += '#' * self.__width
+                if i != self.__height - 1:
+                    rectang += '\n'
+        return rectang
+
+    def __repr__(self):
+        return "Rectangle({}, {})".format(self.__width, self.__height)
