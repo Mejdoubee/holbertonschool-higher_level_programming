65bf8e79038a2bc75339bae3c7b95a9b0c72a4f4
Mejdoubee <6235@holbertonstudents.com>
2023-06-24 13:46:47 +0100

    BaseGeometry class + area()

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''
+BaseGeometry module
+'''
+
+
+class BaseGeometry:
+    '''
+    BaseGeometry class
+    '''
+    def area(self):
+        '''
+        Public instance method: that raises an Exception with message
+        '''
+        raise Exception("area() is not implemented")
