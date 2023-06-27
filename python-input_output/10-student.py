42bd094b55b4dcc036eb4d84cd4922fbb229f167
Mejdoubee <6235@holbertonstudents.com>
2023-06-27 15:13:51 +0100

    Student to JSON with filter

---
+++ python-input_output/10-student.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+'''
+Module that define Student class
+'''
+
+
+class Student:
+    '''
+    class Student that defines a student by:
+    Public instance attributes:
+    first_name, last_name and age
+    '''
+    def __init__(self, first_name, last_name, age):
+        '''
+        Instantiation ...
+        '''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        '''
+        Public method def to_json(self, attrs=None): that retrieves
+        a dictionary representation
+        If attrs is a list of strings, only attribute names contained in
+        this list must be retrieved.
+        Otherwise, all attributes must be retrieved
+        '''
+        if attrs is None:
+            return self.__dict__
+        else:
+            attr_dict = {}
+            for i in attrs:
+                if i in self.__dict__:
+                    attr_dict[i] = self.__dict__[i]
+            return attr_dict
