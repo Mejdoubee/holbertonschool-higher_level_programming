c53f48756ebd77fb6fd27a0ce99fe25a298a651b
Mejdoubee <6235@holbertonstudents.com>
2023-06-29 20:14:38 +0100

    Base class

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''
+Module that define "Base" class
+'''
+
+
+class Base:
+    '''
+    "Base" class
+    '''
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        '''
+        Class constructor
+        '''
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
