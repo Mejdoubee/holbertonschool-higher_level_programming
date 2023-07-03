2b1e11a30e4944aff46d97db10e71f491a2881e5
Mejdoubee <6235@holbertonstudents.com>
2023-07-03 15:33:24 +0100

    Dictionary to JSON string

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -26,4 +26,6 @@ class Base:
         '''
         returns the JSON string representation of list_dictionaries
         '''
+        if list_dictionaries is None or not list_dictionaries:
+            return "[]"
         return json.dumps(list_dictionaries)
