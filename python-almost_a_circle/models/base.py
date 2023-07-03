2bf4e5b4eac238ec093d1f9a9328b613f243a589
Mejdoubee <6235@holbertonstudents.com>
2023-07-03 16:21:37 +0100

    JSON string to file

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -30,6 +30,7 @@ class Base:
             return "[]"
         return json.dumps(list_dictionaries)
 
+    @classmethod
     def save_to_file(cls, list_objs):
         '''
         class method that writes the JSON string representation of
