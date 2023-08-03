8e3e5b8daca2f31f52062d4e7beedad69805df15
Mejdoubee <6235@holbertonstudents.com>
2023-08-04 00:38:31 +0100

    Pascal's Triangle

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+
+    for i in range(1, n):
+        row = [1]
+        last_row = triangle[-1]
+        row += [sum(pair) for pair in zip(last_row, last_row[1:])]
+        row.append(1)
+        triangle.append(row)
+
+    return triangle
