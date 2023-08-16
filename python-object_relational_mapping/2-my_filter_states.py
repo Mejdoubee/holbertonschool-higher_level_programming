4a8e2eb58073e80f0d1bdbd07ecc6d1e561de96a
Mejdoubee <6235@holbertonstudents.com>
2023-08-16 23:24:13 +0100

    Filter states by user input

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -16,8 +16,10 @@ if __name__ == "__main__":
         db=argv[3]
         )
     cur = db.cursor()
-    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
-    cur.execute(query, (argv[4],))
+    query = "SELECT * FROM states WHERE name LIKE '{}' ORDER BY id ASC".format(
+        argv[4]
+        )
+    cur.execute(query)
     rows = cur.fetchall()
     for row in rows:
         print(row)
