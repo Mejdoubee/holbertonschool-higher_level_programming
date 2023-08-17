284a67b9039c1cd44274816971023f8787254967
Mejdoubee <6235@holbertonstudents.com>
2023-08-17 03:45:46 +0100

    Contains

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -11,9 +11,12 @@ from sqlalchemy import create_engine
 
 if __name__ == '__main__':
     engine = create_engine(
-        f'mysql+mysqldb://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}')
+        f'mysql+mysqldb://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}',
+        pool_pre_ping=True
+    )
     Session = sessionmaker(bind=engine)
     session = Session()
-    for state in session.query(State).order_by(State.id):
-        if 'a' in state.name:
-            print(f'{state.id}: {state.name}')
+    for state in session.query(State).filter(
+            State.name.like('%a%')).order_by(State.id):
+        print(f'{state.id}: {state.name}')
+    session.close()
