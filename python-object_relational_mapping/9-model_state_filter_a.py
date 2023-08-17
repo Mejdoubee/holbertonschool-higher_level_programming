70744948456622bb05de20ae8322af3589caf422
Mejdoubee <6235@holbertonstudents.com>
2023-08-17 03:13:56 +0100

    Contains

---
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''
+script that lists all State objects that contain
+the letter 'a' from the database hbtn_0e_6_usa
+'''
+from sys import argv
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from sqlalchemy import create_engine
+
+
+if __name__ == '__main__':
+    engine = create_engine(
+        f'mysql+mysqldb://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}')
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for state in session.query(State).filter(
+            State.name.like('%a%')).order_by(State.id):
+        print(f'{state.id}: {state.name}')
+    session.close()
