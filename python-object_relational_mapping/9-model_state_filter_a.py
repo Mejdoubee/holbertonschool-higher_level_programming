fe5954779af132e7d0c12e5d29b019bb05ea4321
Mejdoubee <6235@holbertonstudents.com>
2023-08-17 03:47:30 +0100

    Contains

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -11,8 +11,7 @@ from sqlalchemy import create_engine
 
 if __name__ == '__main__':
     engine = create_engine(
-        f'mysql+mysqldb://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}',
-        pool_pre_ping=True
+        f'mysql+mysqldb://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}'
     )
     Session = sessionmaker(bind=engine)
     session = Session()
