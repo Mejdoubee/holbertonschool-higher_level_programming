e0742b3d38d097537d1531a9ed77a6d3f798cb71
mejdoubee <6235@holbertonstudents.com>
2023-09-20 23:27:12 +0100

    Toggle classes

---
+++ javascript-dom_manipulation/3-script.js
@@ -0,0 +1,5 @@
+document.getElementById("toggle_header").addEventListener("click", function() {
+	let element = document.querySelector("header");
+	element.classList.toggle("red");
+	element.classList.toggle("green")
+});
