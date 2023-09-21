ef0a77a76b632e5c3ef97e6bb3959c4ca8911fe3
mejdoubee <6235@holbertonstudents.com>
2023-09-21 02:14:58 +0100

    Click and turn red

--- javascript-dom_manipulation/1-script.js
+++ javascript-dom_manipulation/1-script.js
@@ -1,3 +1,5 @@
+// JavaScript script that updates the text color of the header element
+// to red (#FF0000) when the user clicks on the tag with id red_header
 document.getElementById("red_header").addEventListener("click", function() {
 	document.querySelector("header").style.color = "#FF0000";
 });
