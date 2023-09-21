0d93e236b919115b61d574cd303909e277c97ce6
mejdoubee <6235@holbertonstudents.com>
2023-09-21 02:14:01 +0100

    Add .red class

--- javascript-dom_manipulation/2-script.js
+++ javascript-dom_manipulation/2-script.js
@@ -1,3 +1,5 @@
+// JavaScript script that adds the class red to the header element
+// when the user clicks on the tag with id red_header
 document.getElementById("red_header").addEventListener("click", function() {
     document.querySelector("header").classList.add("red");
 });
