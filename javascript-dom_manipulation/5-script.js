f3626e2e67912c15ed76961d4e9dfa26bad5060e
mejdoubee <6235@holbertonstudents.com>
2023-09-21 02:11:38 +0100

    Change the text

--- javascript-dom_manipulation/5-script.js
+++ javascript-dom_manipulation/5-script.js
@@ -1,3 +1,5 @@
+// JavaScript script that updates the text of the header element to New Header!!!
+// when the user clicks on the element with id update_header
 document.getElementById("update_header").addEventListener("click", function() {
 	document.querySelector("header").innerText = "New Header!!!";
 });
