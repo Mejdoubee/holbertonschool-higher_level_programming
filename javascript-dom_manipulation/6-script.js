857273f58a769f1b3b1901b11eb6124673241c9c
mejdoubee <6235@holbertonstudents.com>
2023-09-21 02:10:52 +0100

    fetch name

--- javascript-dom_manipulation/6-script.js
+++ javascript-dom_manipulation/6-script.js
@@ -1,3 +1,5 @@
+// JavaScript script that fetches the character name 
+// from this URL: https://swapi-api.hbtn.io/api/people/5/?format=json
 fetch('https://swapi-api.hbtn.io/api/people/5/?format=json')
 	.then(response => response.json())
 	.then(data => document.getElementById("character").innerText = data.name)
