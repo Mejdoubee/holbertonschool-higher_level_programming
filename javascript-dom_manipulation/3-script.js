41f6a69f1598f5566649adc3af6777ac99723d66
mejdoubee <6235@holbertonstudents.com>
2023-09-21 02:13:04 +0100

    Toggle classes

--- javascript-dom_manipulation/3-script.js
+++ javascript-dom_manipulation/3-script.js
@@ -1,3 +1,5 @@
+// JavaScript script that toggles the class of
+// the header element when the user clicks on the tag id toggle_header
 document.getElementById("toggle_header").addEventListener("click", function() {
 	let element = document.querySelector("header");
 	element.classList.toggle("red");
