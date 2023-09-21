f5936af17adc92dcc0ba6f7e5a2a2cd9e63c8dc5
mejdoubee <6235@holbertonstudents.com>
2023-09-21 02:15:31 +0100

    Color Me

--- javascript-dom_manipulation/0-script.js
+++ javascript-dom_manipulation/0-script.js
@@ -1,3 +1,4 @@
+// JavaScript script that updates the text color of the header element to red (#FF0000)
 let header = document.querySelector('header');
 if (header) {
 	header.style.color = "#FF0000";
