184d6f80730f225e04d636dbe422f399ac4c1739
mejdoubee <6235@holbertonstudents.com>
2023-09-21 02:12:16 +0100

    List of elements

--- javascript-dom_manipulation/4-script.js
+++ javascript-dom_manipulation/4-script.js
@@ -1,3 +1,5 @@
+// JavaScript script that adds a li element to a list
+// when the user clicks on the element with id add_item
 document.getElementById("add_item").addEventListener("click", function() {
 	let Item = document.createElement("li");
 	Item.innerText = "Item";
